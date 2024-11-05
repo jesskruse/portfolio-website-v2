68b086875bbceb80085f86a0a29a7d48206bd614
jesskruse <100980906+jesskruse@users.noreply.github.com>
2024-11-04 16:39:00 -0800

    Added manual darkMode toggle for TailWind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  // https://tailwindcss.com/docs/dark-mode#toggling-dark-mode-manually
+  darkMode: "selector",
   theme: {
     extend: {},
   },
